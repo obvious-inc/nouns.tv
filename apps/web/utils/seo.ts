f0ff60bf61aa15f14a95de035f5bc36804b54f73
prego <pedropregueiro@gmail.com>
2022-08-26 13:22:01 +0200

    BARS

--- apps/web/utils/seo.ts
+++ apps/web/utils/seo.ts
@@ -1,3 +1,3 @@
 export const SITE_DESCRIPTION = "twitch-like experience for nouns";
 export const SITE_TITLE = "nouns.tv";
-export const SITE_URL = "https://nouns-tv.vercel.app/";
+export const SITE_URL = "https://nouns-tv.vercel.app";
